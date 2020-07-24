dadaff074536743bb907369564765d75dd0cee67
JunhoYeo <hanaro0704@gmail.com>
2020-07-24 21:04:47 +0900

    feat(init): create app with cra

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App: React.FC = () => {
+  return (<Container />);
+};
+
+export default App;
+
+const Container = styled.div`
+`;
